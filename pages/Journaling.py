c8beb323ee903a3b8c9178ab5daad1b2d3c2846e
finanazwa <137764298+finanazwa@users.noreply.github.com>
2023-06-27 19:42:56 +0800

    Update Journaling.py

--- pages/Journaling.py
+++ pages/Journaling.py
@@ -1,3 +1,39 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Journal")
+def load_journal():
+    # Load the journal data from a file or database
+    # For demonstration purposes, we'll use a pandas DataFrame
+    journal_data = pd.DataFrame(columns=["Date", "Entry"])
+    return journal_data
+
+def save_journal(journal_data):
+    # Save the journal data to a file or database
+    # For demonstration purposes, we'll simply print the data
+    print(journal_data)
+
+def main():
+    # Load existing journal data or create a new one
+    journal_data = load_journal()
+
+    # Title and introduction
+    st.title("Journal App")
+    st.write("Welcome to your personal journal!")
+
+    # User input for new journal entry
+    entry_date = st.date_input("Date", value=pd.to_datetime("today").date())
+    entry_text = st.text_area("Entry", height=200)
+
+    # Save the entry when the user clicks the button
+    if st.button("Save Entry"):
+        journal_data = journal_data.append({"Date": entry_date, "Entry": entry_text}, ignore_index=True)
+        save_journal(journal_data)
+        st.success("Entry saved successfully!")
+
+    # Display existing journal entries
+    st.subheader("Journal Entries")
+    st.dataframe(journal_data)
+
+if __name__ == "__main__":
+    main()
+
